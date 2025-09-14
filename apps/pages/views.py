7af4fe44b0205c6ccd38986520dca83bcd7a1801
nishan2 <nishanpaudel9090@gmail.com>
2025-09-15 08:38:23 +1000

    update-> menulist updated

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -8,3 +8,9 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+@login_required(login_url='/login/')
+def members(request):
+    return render(request, 'pages/members.html')
+
+    
--- config/urls.py
+++ config/urls.py
@@ -20,11 +20,14 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('apps.pages.urls')),
+    path('/members', include('apps.pages.urls')),
     path('', include('apps.dyn_dt.urls')),
     path('', include('apps.dyn_api.urls')),
     path('charts/', include('apps.charts.urls')),
     path("admin/", admin.site.urls),
     path("", include('admin_adminlte.urls')),
+    path('', include('apps.pages.urls')),  # handles / and /members/
+    path('admin/', admin.site.urls),
 
 
     # login / logout
