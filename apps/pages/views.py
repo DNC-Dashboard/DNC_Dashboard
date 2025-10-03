4a7d8bc993cde332372ce204bf8a8a7d897c852f
Rahul Korassery Radhakrishnan <krrahulrk9@gmail.com>
2025-10-03 12:42:59 +1000

    Update views.py

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,54 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 
-# Create your views here.
 @login_required(login_url='/login/')
 def index(request):
-
-    # Page from the theme 
     return render(request, 'pages/index.html')
 
 @login_required(login_url='/login/')
 def members(request):
     return render(request, 'pages/members.html')
 
-
-
-
-# Create your views below 👇👇👇
-
-# Analytics features
-    
+# Analytics
+@login_required(login_url='/login/')
 def analytics(request):
     return render(request, "pages/analytics.html")
 
-
-
-    
-def analytics(request):
-    return render(request, "pages/analytics.html")
-
+@login_required(login_url='/login/')
 def task_management(request):
     return render(request, "pages/kanban.html")
+
+@login_required(login_url='/login/')
 def projects(request):
     return render(request, "pages/projects.html")
 
-    
+@login_required(login_url='/login/')
 def campaigns(request):
     return render(request, "pages/campaigns.html")
 
- 
-def nomand(request):
+# Profile (this fixes the {% url 'profile' %} error)
+@login_required(login_url='/login/')
+def profile(request):
     return render(request, "pages/profile.html")
 
- 
+@login_required(login_url='/login/')
 def configuration(request):
     return render(request, "pages/configuration.html")
 
+@login_required(login_url='/login/')
 def assets(request):
     return render(request, "pages/assets.html")
-
-
-
-    
