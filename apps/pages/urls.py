126e475973020f81ff67ae2a6458ac43012235bd
Rahul Korassery Radhakrishnan <krrahulrk9@gmail.com>
2025-10-03 12:49:08 +1000

    UPdate

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-  
+
     path('analytics/', views.analytics, name='analytics'),
     path('task_management/', views.task_management, name='task_management'),
     path('campaigns/', views.campaigns, name='campaigns'),
-    path('nomand/', views.nomand, name='nomand'),
+    path('profile/', views.profile, name='profile'),   # ✅ updated
 
     path('configuration/', views.configuration, name='configuration'),
-   
     path('projects/', views.projects, name='projects'),
     path('assets/', views.assets, name='assets'),
-
-   
 ]
