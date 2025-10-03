38175932ecc1ca79e7fde38ef11a3b8c3ccf9a12
Nishan Paudel <31773979+Nishan99@users.noreply.github.com>
2025-10-03 11:40:50 +1000

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path("accounts/", include(("django.contrib.auth.urls", "auth"), namespace="auth")),
 
  path("login/", auth_views.LoginView. as_view(template_name="accounts/login.html"), name="login"), 
-path("logout/", auth_views.LogoutView as_view(next-page="login"), name="logout"),
+path("logout/", auth_views.LogoutView as_view(next_page="login"), name="logout"),
 
 ]
